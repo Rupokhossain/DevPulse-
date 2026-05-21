3c56894c522f8068a9abde5246a49e4c61e0adfe
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-05-21 14:23:18 +0600

    Initialize Express TypeScript project

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+
+import { Pool } from "pg";
+import config from "../config";
+
+
+export const pool = new Pool({
+    connectionString: config.connection_string,
+})
+
+
+export const initDB = async () => {
+    try {
+            console.log("database connected successfully!");
+    } catch (error) {
+        console.log(error)
+    }
+}
